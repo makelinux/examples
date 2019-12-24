c8ee8874a095f1598d1604e412d5c55936c2fc00
Costa Shulyupin <constantine.shulyupin@gmail.com>
2019-12-24 19:40:14 +0200

    *20

--- cpp/20.cpp
+++ cpp/20.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <vector>
+#include <string_view>
+#include <cmath>
 #include <cassert>
+#include <vector>
+#include <iostream>
+
+static_assert(__cplusplus == 201707);
+
 
 using namespace std;
+
+/**
+  https://en.cppreference.com/w/cpp/ranges
+  https://en.cppreference.com/w/cpp/utility/source_location
+  https://en.cppreference.com/w/cpp/language/coroutines
+  */
 
 int main()
 {
