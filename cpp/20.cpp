a9ac5bfd0a634825b468774596d015f645a02530
Costa Shulyupin <constantine.shulyupin@gmail.com>
2020-01-19 21:41:40 +0200

    concepts, requires

--- cpp/20.cpp
+++ cpp/20.cpp
@@ -45,7 +45,36 @@ void init_20()
 	assert(l3.b.y == 4);
 }
 
+#if __cpp_concepts
+
+// https://en.wikipedia.org/wiki/Concepts_(C++)
+// https://en.cppreference.com/w/cpp/language/constraints
+// https://github.com/AnthonyCalandra/modern-cpp-features/blob/master/CPP20.md
+
+// Using 'requires':
+
+template<typename T>
+requires is_same_v<T, int>
+T inc(T a)
+{ return a + 1; };
+
+template<typename T>
+T inc(T a)
+requires is_same_v<T, string>
+{ return a + "1"; };
+
+void concept_demo()
+{
+	assert(inc<int>(1) == 2);
+	assert(inc<string>("1") == "11");
+}
+
+#else
+void concept_demo() {}
+#pragma message("undefined __cpp_concepts")
+#endif
 int main()
 {
 	init_20();
+	concept_demo();
 }
