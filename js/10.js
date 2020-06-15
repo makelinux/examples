9c3fd8e2e8fa371c442e00b863374c934da71a85
Costa Shulyupin <constantine.shulyupin@gmail.com>
2020-06-15 21:28:33 +0300

    ^10

--- js/10.js
+++ js/10.js
@@ -1,5 +1,32 @@
-// ES10 - ECMAScript 2019
-// https://medium.com/@selvaganesh93/javascript-whats-new-in-ecmascript-2019-es2019-es10-35210c6e7f4b
+// ES10 - ECMAScript 2019 examples
 
-// Array.prototype.flat, Array.prototype.flatMap, changes to Array.sort and Object.fromEntries.[10]
-// TODO
+assert_eq([ 0, [ 1 ] ].flat(), [0, 1])
+
+assert_eq([ 0, [ 1, [ 2 ] ] ].flat(), [ 0, 1, [ 2 ] ])
+
+assert_eq([ 0, [ 1, [ 2 ] ] ].flat(Infinity), [ 0, 1, 2 ])
+
+assert_eq([ 0, 2 ].flatMap(x => [x, x + 1]), [ 0, 1, 2, 3 ])
+
+assert_eq(" a ".trimStart().trimEnd(), "a")
+
+try  { } catch /* Optional Catch Binding */ { }
+
+assert_eq(Object.fromEntries([['1', 2], ['3', 4]]), { '1': 2, '3': 4 })
+
+assert_eq((function () { }).toString(), 'function () { }')
+
+assert_eq(String(Symbol(1)), 'Symbol(1)')
+
+assert_eq(Symbol(1).description, '1')
+
+assert_eq(JSON.stringify('\uD800'), '"\\ud800"')
+
+/*
+ References
+ https://alligator.io/js/es2019/
+ https://blog.logrocket.com/5-es2019-features-you-can-use-today/
+ https://firstclassjs.com/ecmascript-2019es2019-new-features-with-examples/
+ https://medium.com/@selvaganesh93/javascript-whats-new-in-ecmascript-2019-es2019-es10-35210c6e7f4b
+*/
+
