447c2d0b0b8e440c8717760868a8337d96458df2
Costa Shulyupin <constantine.shulyupin@gmail.com>
2020-06-15 21:28:47 +0300

    ^9

--- js/9.js
+++ js/9.js
@@ -1,19 +1,31 @@
-// ES9 - ECMAScript 2018
-// https://medium.com/@bramus/javascript-whats-new-in-ecmascript-2018-es2018-17ede97f36d5
+// ES9 - ECMAScript 2018 examples
 
-// https://github.com/tc39/proposal-object-rest-spread
+[a, ...rest1] = [1, 2, 3];
 
-[ a, ...rest1 ] = [ 1, 2, 3 ];
+assert_eq(rest1, [2, 3])
 
-assert_eq(rest1, [ 2, 3 ])
+let {b, ...rest2} = { b: 1, c:2, d:3};
+assert_eq(rest2, { c: 2, d: 3});
 
-let {b, ...rest2} = { b: 1, c:2, d:3 };
-assert_eq(rest2, { c: 2, d: 3 });
-
-assert_eq([ 1, 2, ...[ 3, 4 ] ], [ 1, 2, 3, 4 ]);
+assert_eq([1, 2, ...[3, 4]], [1, 2, 3, 4]);
 
 // https://github.com/tc39/proposal-async-iteration
+var buff = '0'
+async function process(array) {
+  for await (let i of array) {
+	  buff += String(i)
+	  throw 'e'
+  }
+}
 
-// Promise.prototype.finally() and additions to RegExp
+process([1, 2])
+	.catch(a => {buff += a})
+	.finally(() => {buff += 'f';
+		assert_eq(buff, '01ef')});
 
-// TODO
+/* Node.js only:
+assert_eq(/(?<name>.)/.exec('1').groups.name, '1')
+*/
+
+// https://medium.com/@bramus/javascript-whats-new-in-ecmascript-2018-es2018-17ede97f36d5
+// https://github.com/tc39/proposal-object-rest-spread
