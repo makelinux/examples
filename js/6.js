9104c711e1345df4d51e8ee43a8086c8b675fc9e
Costa Shulyupin <constantine.shulyupin@gmail.com>
2020-06-17 20:24:01 +0300

    ^6

--- js/6.js
+++ js/6.js
@@ -1,5 +1,8 @@
 // ES6
 // https://github.com/lukehoban/es6features
 // https://www.smashingmagazine.com/2015/10/es6-whats-new-next-version-javascript/
+// https://www.w3schools.com/js/js_es6.asp
+// https://davidwalsh.name/es6-features
+// https://davidwalsh.name/es6-features-ii
 
 // TODO
