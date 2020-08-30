cddb24dc18868cf2b521412fa89fdf5c40818725
Costa Shulyupin <constantine.shulyupin@gmail.com>
2020-08-30 13:44:39 +0300

    C11

---
+++ c/11.c
@@ -0,0 +1,67 @@
+_Static_assert(__STDC_VERSION__ > 201100, "");
+
+/**
+  https://gcc.gnu.org/wiki/C11Status
+
+  */
+#include <stdnoreturn.h>
+#include <stdatomic.h>
+#include <math.h>
+#include <assert.h>
+#include <stdio.h>
+
+#if !__STDC_NO_THREADS__
+#include <threads.h>
+
+/**
+
+  https://en.cppreference.com/w/c/thread
+
+  https://www.gnu.org/software/libc/manual/html_node/ISO-C-Threads.html
+  */
+
+int run(void *arg)
+{
+	*(int *)arg = 1;
+}
+
+int thrd_passed;
+
+_Static_assert(thrd_success == 0, "");
+
+void thrd_test()
+{
+	thrd_t thread;
+	int res;
+	int passed = 0;
+	res = thrd_create(&thread, run, &passed);
+	assert(!res);
+	assert(res == thrd_success);
+
+	thrd_join(thread, NULL);
+	assert(passed);
+}
+
+#else
+
+#pragma message "thrd_test skipped"
+void thrd_test()
+{
+}
+
+#endif
+
+#define cbrt(x) _Generic((x), long double: cbrtl, \
+                              default: cbrt, \
+                              float: cbrtf)(x)
+
+int main()
+{
+	assert(__STDC_VERSION__ > 201100);
+	atomic_int acnt;
+	++acnt; // is thread safe
+	assert(cbrt(1.1 * 1.1 * 1.1) == 1.1);
+	assert(cbrt(8) == 2);
+	thrd_test();
+	return 0;
+}
--- c/README.md
+++ c/README.md
@@ -1,8 +1,9 @@
 # C features examples by standard version
-* [Legacy "Traditional"](tr.c)
-* [ANSI C / C89](ansi.c)
-* [C99](99.c)
+* [C11, ISO 9899:2011](11.c)
 * [libc example](libc.c)
+* [C99, ISO 9899:1999](99.c)
+* [ANSI C / C89, ISO 9899:199409](ansi.c)
+* [Legacy "Traditional"](tr.c)
 
 References
 * [Language Standards Supported by GCC](https://gcc.gnu.org/onlinedocs/gcc/Standards.html)
