f58ad2b1263a94343bb6e604aef00ac288da2841
Costa Shulyupin <constantine.shulyupin@gmail.com>
2021-07-05 22:42:38 +0300

    *visitor_demo

--- cpp/patterns.cpp
+++ cpp/patterns.cpp
@@ -1,71 +1,69 @@
 #include <memory>
 #include <list>
 
-#define trace(end) cerr << __func__ << end;
-
 /**
   @file
-
-  Call hierarchy:
-
-  visitor_demo
-  	client_visit
-		component_accept
-			visit
-				component_method
 */
 
 struct A_component;
 
 struct Visitor {
-	virtual int visit(const A_component&) const = 0;
+	virtual string visit(const A_component&) const = 0;
 };
 
 struct Component {
-	virtual int component_accept(Visitor&) const = 0;
+	virtual string component_accept(Visitor&) const = 0;
 };
 
 /// @brief knows only abstract visitor and component
-void client_visit(const list<unique_ptr<Component>>& components,
-		  Visitor& visitor)
+string client_visit(const list<unique_ptr<Component>>& components,
+		Visitor& visitor)
 {
-	int rc = 0;
-	trace(" > ");
+	string res;
 	for (auto&& comp : components) {
-		rc = comp->component_accept(visitor);
-		assert(rc);
+		res += string(__func__) + " > " + comp->component_accept(visitor);
+		//assert(rc);
 	}
-	assert(rc);
+	//assert(rc);
+	return res;
 }
 
 /// @brief one of many components
 struct A_component : public Component {
-	int component_accept(Visitor& visitor) const override {
-		trace(" > ");
-		return visitor.visit(*this);
+	string component_accept(Visitor& visitor) const override {
+		return string(__func__) + " > " + visitor.visit(*this);
 	}
 	/// @brief is not virtual
-	int component_method() const {
-		trace("\n");
-		return 1;
+	string component_method() const {
+		return __func__;
 	}
 };
 
+/**
+  Call hierarchy:
+
+	visitor_demo
+		client_visit
+			component_accept
+				visit
+					component_method
+*/
+
 void visitor_demo()
 {
-	trace(" > ");
 	/// @brief one of many visitors
 	struct A_visitor : public Visitor {
-		int visit(const A_component& c) const override {
-			trace(" > ");
-			return c.component_method();
+		string visit(const A_component& c) const override {
+			return string(__func__) + " > " + c.component_method();
 		}
 	};
 
 	list<unique_ptr<Component>> components;
 	components.emplace_back(new A_component);
 	A_visitor v;
-	client_visit(components, v);
+	assert(client_visit(components, v) ==
+		"client_visit > component_accept > visit > component_method");
+}
 }
 
 int main()
