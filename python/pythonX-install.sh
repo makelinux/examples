a58d79f757daf4b43ec39ccc02365e523c2d0d31
Costa Shulyupin <constantine.shulyupin@gmail.com>
2022-08-23 18:22:40 +0300

    ^python

--- python/pythonX-install.sh
+++ python/pythonX-install.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-grep --quiet deadsnakes/ppa --recursive /etc/apt/sources.list.d/ || sudo add-apt-repository ppa:deadsnakes/ppa
+# https://launchpad.net/~deadsnakes/+archive/ubuntu/ppa
+
+grep --quiet '^deb.*deadsnakes/ppa' --recursive /etc/apt/sources.list.d/ || sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt-get -q update
 
-for v in 2.{3..7} 3.{1..9}; do
+for v in 2.{3..7} 3.{1..10}; do
 	which python$v || sudo apt-get --yes install python$v
 done
 
