30ee3e67dcae1f15a46c50a8e2fba1d25a1a7788
Costa Shulyupin <constantine.shulyupin@gmail.com>
2026-01-26 19:29:15 +0200

    LlamaStackClient()

--- llm/llama-stack-client-completions-min.py
+++ llm/llama-stack-client-completions-min.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+# Prerequisites:
+# export LLAMA_STACK_CLIENT_BASE_URL=http://localhost:8321
 # export INFERENCE_MODEL=ollama/llama3.2:3b
 # ollama run $INFERENCE_MODEL
 # llama stack run starter
@@ -7,7 +9,7 @@
 import os
 from llama_stack_client import LlamaStackClient
 
-c = LlamaStackClient(base_url="http://localhost:8321")
+c = LlamaStackClient()
 
 r = c.chat.completions.create(
     model=os.getenv("INFERENCE_MODEL", "ollama/llama3.2:3b"),
